7a8178d6ce37c2f151b4d852d81b3d9468a67f76
Olivier Fondeur <fondeurolivier14@gmail.com>
2025-03-13 12:32:39 -0700

    plots for PTB: bad result bcs of not enough data, plus take only the drugs

--- make_prediction.py
+++ make_prediction.py
@@ -21,7 +21,21 @@ def predict_from_unstim_data(
     # load the data to predict and filter with the interzsting markers
     unstim_anndata_to_predict = ad.read(unstim_data_path)
 
-    features = read_list(config.data.features)
+    # features = read_list(config.data.features)
+    features = [
+        "151Eu_pP38",
+        "149Sm_pCREB",
+        "159Tb_pMK2",
+        "167Er_pERK",
+        "155Gd_pS6",
+        "166Er_pNFkB",
+        "150Nd_pSTAT5",
+        "153Eu_pSTAT1",
+        "154Sm_pSTAT3",
+        "164Er_IkB",
+        "168Er_pSTAT6",
+    ]
+
     unstim_anndata_to_predict = unstim_anndata_to_predict[:, features].copy()
     unstim_anndata_to_predict = unstim_anndata_to_predict[
         unstim_anndata_to_predict.obs["condition"] == "control"
@@ -54,7 +68,7 @@ def predict_from_unstim_data(
 
 
 # tests
-result_path = "results/LPS_cMC_IC1/model-cellot"
-unstim_data_path = "datasets/sherlock_training_data/LPS_cMC_IC1.h5ad"
-output_path = "results/LPS_cMC_IC1/model-cellot/pred.csv"
+result_path = "results/PTB/LPS_cMC/model-cellot"
+unstim_data_path = "datasets/PTB_training/LPS_cMC_HV01.h5ad"
+output_path = "results/PTB/LPS_cMC/model-cellot/pred.csv"
 ada = predict_from_unstim_data(result_path, unstim_data_path, "csv", output_path)
